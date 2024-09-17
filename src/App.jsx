509328fac8fce592b77f8a44f4e2a15f363e8038
Christian Larrea <crlarrea@protonmail.com>
2024-09-17 06:28:52 +0200

    add new marquee row and reverse

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,44 @@ function App() {
           <li>6</li>
         </ul>
       </div>
+      <div className="marquee">
+        <ul className="marquee__content">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+        </ul>
+
+        <ul aria-hidden="true" className="marquee__content">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+        </ul>
+      </div>
+      <div className="marquee">
+        <ul className="marquee__content">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+        </ul>
+
+        <ul aria-hidden="true" className="marquee__content">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+        </ul>
+      </div>
     </>
   );
 }
